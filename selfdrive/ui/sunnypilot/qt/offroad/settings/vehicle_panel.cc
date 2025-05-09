68fbdd69749fa8ecfa70867acf5c60dd17ee9f2b
discountchubbs <alexgrant990@gmail.com>
2025-05-09 08:35:18 -0700

    update settings

--- selfdrive/ui/sunnypilot/qt/offroad/settings/vehicle_panel.cc
+++ selfdrive/ui/sunnypilot/qt/offroad/settings/vehicle_panel.cc
@@ -41,7 +41,7 @@ void VehiclePanel::showEvent(QShowEvent *event) {
 
 void VehiclePanel::updatePanel(bool _offroad) {
   platformSelector->refresh(_offroad);
-
+  hyundaiSettings->updateSettings(_offroad);
   updateBrandSettings();
 
   offroad = _offroad;
